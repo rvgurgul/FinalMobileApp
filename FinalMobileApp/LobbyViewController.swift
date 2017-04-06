d1921ff9e34b0188a0444d13b35f838bb8822180
Richie <legorvg9@gmail.com>
2017-04-06 09:58:30 -0500

    Sorta started the storyboard stuff

--- FinalMobileApp/LobbyViewController.swift
+++ FinalMobileApp/LobbyViewController.swift
@@ -16,18 +16,29 @@ enum GameType: String
 class LobbyViewController: UIViewController
 {
     var gameType: GameType!
+    var settings: [String:Any]!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        gameType = .HideAndBeac
+        settings = defaultSettingsFor(game: gameType)
+    }
     
     func settingsButton()
     {
         let alert = UIAlertController(title: "Settings Menu", message: nil, preferredStyle: .actionSheet)
         
-        for setting in settingsFor(game: .HideAndBeac)
+        for setting in defaultSettingsFor(game: .HideAndBeac)
         {
-            alert.addAction(UIAlertAction(title: "\(setting.key): \(setting.value)", style: .default, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>))
+            //alert.addAction(UIAlertAction(title: "\(setting.key): \(setting.value)", style: .default, handler: <#T##((UIAlertAction) -> Void)?##((UIAlertAction) -> Void)?##(UIAlertAction) -> Void#>))
+            //programatically add the handlers.
+            
         }
     }
     
-    func settingsFor(game: GameType) -> [String:Any]
+    func defaultSettingsFor(game: GameType) -> [String:Any]
     {
         switch game
         {
